305790efd773a93654f405a717578659286661f8
Ivan Bustos <rockerrobot.tech@gmail.com>
2025-06-09 17:31:43 -0400

    Realiza cambios para funcionamiento del contenedor

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : '/internal/',
   plugins: [vue(), vueDevTools()],
   server: {
     host: mode === 'development', // Solo establece host en desarrollo
